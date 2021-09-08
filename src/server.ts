dd4208440ff809ff14a52636a268d65907c71dd7
Nam Hoang (particle4dev) <particle4dev@gmail.com>
2021-09-08 12:26:05 +0700

    fix error Property 'session' does not exist on type 'Request'

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 process.env['NODE_CONFIG_DIR'] = __dirname + '/configs';
 
 import 'dotenv/config';
+import session from 'express-session';
 import App from '@/app';
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
+
+declare module 'express-session' {
+  export interface SessionData {
+    userid: any;
+  }
+}
 
 validateEnv();
 
